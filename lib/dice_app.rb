67aa25592985b36c6f1b790f46145bf856e14fde
Robert <bibbycodes@gmail.com>
2019-10-18 12:14:55 +0100

    adds score methods and test

--- lib/dice_app.rb
+++ lib/dice_app.rb
@@ -1,6 +1,23 @@
-class Dice
-  
+class Dice 
   def roll
     return rand(1..6)
+  end
+end
+
+class Game
+  def initialize
+    @record = []
+  end
+
+  def record(roll)
+    @record << roll
+  end
+
+  def score
+    total = 0
+    for record in @record
+      total += record
+    end
+    return total
   end
 end
--- spec/dice_app_spec.rb
+++ spec/dice_app_spec.rb
@@ -2,13 +2,31 @@ require 'dice_app'
 
 describe Dice do
   context "When Using the Dice Class" do
-    it "should return a radom integer between 1 and 6 when using the roll dice method" do
-    end
-
     it "should return a number between 1 and 6" do
       dice = Dice.new
       expect(dice.roll).to be_between(1,6)
     end
 
+  end
+end
+
+describe Game do
+  context "When using the Game" do
+    it {is_expected.to respond_to(:record)} 
+
+    it "Should record dice rolls" do
+      dice = Dice.new
+      roll = dice.roll
+      expect(subject.record(roll)).to eq(subject.instance_variable_get(:@record))
+    end
+
+    it "Should return the current score for any game" do
+      dice = Dice.new
+      roll_1 = dice.roll
+      subject.record(roll_1)
+      roll_2 = dice.roll
+      subject.record(roll_2)
+      expect(subject.score).to eq(roll_1 + roll_2)
+    end
   end
 end
